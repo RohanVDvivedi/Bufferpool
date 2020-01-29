4532f3d5c7fa16b03e637e9342c26e9c92960aef
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-29 21:58:05 +0100

    made simple implementation to the page_entry function implementation function

--- inc/page_entry.h
+++ inc/page_entry.h
@@ -7,14 +7,20 @@
 
 #include<rwlock.h>
 
+#include<disk_access_functions.h>
+
 // in general
 // page_id = block_id / blocks_count
+// page_size (in bytes) = blocks_count * block_size (in bytes)
 
 typedef struct page_entry page_entry;
 struct page_entry
 {
 	// this lock ensures only 1 thread attempts to read or write the page to the disk
 	rwlock* page_entry_lock;
+
+	// this is the database file discriptor
+	int db_fd;
 
 	// this is the identifier for this page
 	uint32_t page_id;
@@ -23,11 +29,11 @@ struct page_entry
 	// remember: every page is built of same number of consecutive blocks
 	uint32_t block_id;
 
-	// this is the number of blocks, that make up this page
+	// this is the number of blocks, that makes up this page
 	uint32_t blocks_count;
 
-	// this is the number of blocks, that make up this page
-	uint32_t blocks_size;
+	// this is the size of each block in bytes, that makes up this page
+	uint32_t block_size;
 
 	// pointer to the in memory copy of the page
 	void* page;
@@ -44,7 +50,13 @@ struct page_entry
 	uint8_t priority;
 };
 
-page_entry* get_page_entry(uint32_t blocks_count, uint32_t blocks_size, void* page);
+page_entry* get_page_entry(int db_fd, uint32_t block_size, void* page);
+
+// below method helps us, point this page_entry to
+// a given page whose id is page_id, its first block is at block_id 
+// and it has blocks_count number of consecutive blocks,
+// each of which has block_size number of bytes
+void init_page_entry(page_entry* page_ent, uint32_t page_id, uint32_t block_id, uint32_t blocks_count);
 
 int read_page_from_disk(page_entry* page_ent);
 
--- src/page_entry.c
+++ src/page_entry.c
@@ -1,1 +1,45 @@
 #include<page_entry.h>
+
+page_entry* get_page_entry(int db_fd, uint32_t block_size, void* page)
+{
+	page_entry* page_ent = (page_entry*) malloc(sizeof(page_entry));
+
+	page_ent->page_entry_lock = get_rwlock();
+
+	page_ent->db_fd = db_fd;
+
+	page_ent->page_id = 0;
+	page_ent->block_id = 0;
+	page_ent->blocks_count = 0;
+
+	page_ent->block_size = block_size;
+
+	page_ent->page = page;
+
+	page_ent->is_dirty = 0;
+	page_ent->priority = 0;
+
+	return page_ent;
+}
+
+void init_page_entry(page_entry* page_ent, uint32_t page_id, uint32_t block_id, uint32_t blocks_count)
+{
+	page_ent->page_id = page_id;
+	page_ent->block_id = block_id;
+	page_ent->blocks_count = blocks_count;
+}
+
+int read_page_from_disk(page_entry* page_ent)
+{
+	return read_blocks(page_ent->db_fd, page_ent->page, page_ent->block_id, page_ent->block_size, page_ent->blocks_count);
+}
+
+int write_page_to_disk(page_entry* page_ent)
+{
+	return write_blocks(page_ent->db_fd, page_ent->page, page_ent->block_id, page_ent->block_size, page_ent->blocks_count);
+}
+
+void delete_page_entry(page_entry* page_ent)
+{
+	free(page_ent);
+}
