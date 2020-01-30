a4236404fc5d8ec9d825080943c6231723052cbf
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-01-30 11:06:37 +0100

    minor changes

--- inc/dbfile.h
+++ inc/dbfile.h
@@ -4,6 +4,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include<sys/types.h>
 #include<sys/stat.h>
 
 #include<disk_access_functions.h>
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,5 +1,7 @@
 cd ..
 make clean all
 cd test
+stat -f "block_count=%b,  block_size=%k, size=%z" test.db 
 gcc -o test_page_entry.out test_page_entry.c -I$BUFFER_POOL_MAN_PATH/inc -I$CUTLERY_PATH/inc -I$RWLOCK_PATH/inc -L$BUFFER_POOL_MAN_PATH/bin -lbufferpoolman -L$RWLOCK_PATH/bin -lrwlock
 ./test_page_entry.out
+stat -f "block_count=%b,  block_size=%k, size=%z" test.db 
