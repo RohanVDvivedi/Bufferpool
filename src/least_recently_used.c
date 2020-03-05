341aa28f71e4cab91e5fc33c2b81b3eb82f8a6ef
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-03-05 20:20:38 +0530

    minimal lru thread protection introduced

--- src/least_recently_used.c
+++ src/least_recently_used.c
@@ -18,37 +18,62 @@ page_entry* get_replacable_page(lru* lru_p)
 	page_entry* page_ent = NULL;
 	if(lru_p->clean_page_entries->tail != NULL)
 	{
-		page_ent = (page_entry*) get_tail_data(lru_p->clean_page_entries);
-		remove_tail(lru_p->clean_page_entries);
-		page_ent->external_lru_reference = NULL;
+		write_lock(lru_p->clean_page_entries_lock);
+			page_ent = (page_entry*) get_tail_data(lru_p->clean_page_entries);
+			write_lock(page_ent->page_entry_lock);
+				remove_tail(lru_p->clean_page_entries);
+				page_ent->external_lru_reference = NULL;
+			write_unlock(page_ent->page_entry_lock);
+		write_unlock(lru_p->clean_page_entries_lock);
 	}
 	else if(lru_p->dirty_page_entries->tail != NULL)
 	{
+		write_lock(lru_p->dirty_page_entries_lock);
 		page_ent = (page_entry*) get_tail_data(lru_p->dirty_page_entries);
-		remove_tail(lru_p->dirty_page_entries);
-		page_ent->external_lru_reference = NULL;
+			write_lock(page_ent->page_entry_lock);
+				remove_tail(lru_p->dirty_page_entries);
+				page_ent->external_lru_reference = NULL;
+			write_unlock(page_ent->page_entry_lock);
+		write_unlock(lru_p->dirty_page_entries_lock);
 	}
 	return page_ent;
 }
 
 void remove_from_lru(lru* lru_p, page_entry* page_ent)
 {
-	if(page_ent->external_lru_reference != NULL)
-	{
-		if(page_ent->is_dirty)
+	read_lock(page_ent->page_entry_lock);
+		if(page_ent->external_lru_reference == NULL)
 		{
-			remove_node(lru_p->dirty_page_entries, page_ent->external_lru_reference);
+			read_unlock(page_ent->page_entry_lock);
+			return;
 		}
-		else
-		{
-			remove_node(lru_p->clean_page_entries, page_ent->external_lru_reference);
-		}
-		page_ent->external_lru_reference = NULL;
-	}
+	read_unlock(page_ent->page_entry_lock);
+
+	write_lock(lru_p->clean_page_entries_lock);
+		write_lock(page_ent->page_entry_lock);
+			if(page_ent->external_lru_reference != NULL && !page_ent->is_dirty)
+			{
+				remove_node(lru_p->clean_page_entries, page_ent->external_lru_reference);
+				page_ent->external_lru_reference = NULL;
+			}
+		write_unlock(page_ent->page_entry_lock);
+	write_unlock(lru_p->clean_page_entries_lock);
+
+	write_lock(lru_p->dirty_page_entries_lock);
+		write_lock(page_ent->page_entry_lock);
+			if(page_ent->external_lru_reference != NULL && page_ent->is_dirty)
+			{
+				remove_node(lru_p->dirty_page_entries, page_ent->external_lru_reference);
+				page_ent->external_lru_reference = NULL;
+			}
+		write_unlock(page_ent->page_entry_lock);
+	write_unlock(lru_p->dirty_page_entries_lock);
 }
 
 void mark_as_recently_used(lru* lru_p, page_entry* page_ent)
 {
+	remove_from_lru(lru_p, page_ent);
+
 	if(page_ent->is_dirty)
 	{
 		insert_head(lru_p->dirty_page_entries, page_ent);
