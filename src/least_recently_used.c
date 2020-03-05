2d135856770f9c0e00e29b11ad2c9b4e669be89e
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-03-05 19:20:08 +0530

    implemented non thread safe, lru, need locks git status!

--- src/least_recently_used.c
+++ src/least_recently_used.c
@@ -15,14 +15,50 @@ lru* get_lru()
 
 page_entry* get_replacable_page(lru* lru_p)
 {
+	page_entry* page_ent = NULL;
+	if(lru_p->clean_page_entries->tail != NULL)
+	{
+		page_ent = (page_entry*) get_tail_data(lru_p->clean_page_entries);
+		remove_tail(lru_p->clean_page_entries);
+		page_ent->external_lru_reference = NULL;
+	}
+	else if(lru_p->dirty_page_entries->tail != NULL)
+	{
+		page_ent = (page_entry*) get_tail_data(lru_p->dirty_page_entries);
+		remove_tail(lru_p->dirty_page_entries);
+		page_ent->external_lru_reference = NULL;
+	}
+	return page_ent;
 }
 
 void remove_from_lru(lru* lru_p, page_entry* page_ent)
 {
+	if(page_ent->external_lru_reference != NULL)
+	{
+		if(page_ent->is_dirty)
+		{
+			remove_node(lru_p->dirty_page_entries, page_ent->external_lru_reference);
+		}
+		else
+		{
+			remove_node(lru_p->clean_page_entries, page_ent->external_lru_reference);
+		}
+		page_ent->external_lru_reference = NULL;
+	}
 }
 
 void mark_as_recently_used(lru* lru_p, page_entry* page_ent)
 {
+	if(page_ent->is_dirty)
+	{
+		insert_head(lru_p->dirty_page_entries, page_ent);
+		page_ent->external_lru_reference = lru_p->dirty_page_entries->head;
+	}
+	else
+	{
+		insert_head(lru_p->clean_page_entries, page_ent);
+		page_ent->external_lru_reference = lru_p->clean_page_entries->head;
+	}
 }
 
 void delete_lru(lru* lru_p)
