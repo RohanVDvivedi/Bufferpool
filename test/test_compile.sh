ec95fd15f94314efee053091fdc33a2424193333
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-03-11 21:43:31 +0530

    moved back to using and creating test database file in the test directory while running tests, to reduce dependencies

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,7 +1,7 @@
 cd ..
 make clean all
 cd test
-FILE_NAME="/media/rohan/5699ddb0-77df-47e2-8fd0-1152949f0981/test.db"
+FILE_NAME="./test.db"
 gcc -o test_bpm.out test_bpm.c -I$BUFFER_POOL_MAN_PATH/inc -I$CUTLERY_PATH/inc -I$RWLOCK_PATH/inc -L$BUFFER_POOL_MAN_PATH/bin -L$CUTLERY_PATH/bin -L$RWLOCK_PATH/bin -lbufferpoolman -lcutlery -lrwlock
 sudo ./test_bpm.out $FILE_NAME
 stat $FILE_NAME
