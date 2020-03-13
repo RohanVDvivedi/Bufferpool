f88740fbfeb805221232b2c9403ffb4a031929f1
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-03-13 13:15:48 +0530

    refactoring test code, next step will be to debug the thread safety issues, and prior data clean up by the lru structure methods

--- test/test_bpm.c
+++ test/test_bpm.c
@@ -10,6 +10,12 @@
 #define PAGES_IN_HEAP_FILE 20
 #define MAX_PAGES_IN_BUFFER_POOL 3
 
+#define PAGE_DATA_FORMAT "Hello World, This is page number %u -> Buffer pool manager works, %d writes completed..."
+
+bufferpool* bpm = NULL;
+void page_read_and_print(uint32_t page_id);
+void page_write_and_print(uint32_t page_id);
+
 int main(int argc, char **argv)
 {
 	printf("\n\ntest started\n\n");
@@ -19,34 +25,17 @@ int main(int argc, char **argv)
 	{
 		strcpy(file_name, argv[1]);
 	}
-	bufferpool* bpm = get_bufferpool(file_name, MAX_PAGES_IN_BUFFER_POOL, BLOCKS_PER_PAGE);
+	bpm = get_bufferpool(file_name, MAX_PAGES_IN_BUFFER_POOL, BLOCKS_PER_PAGE);
 
 	printf("Bufferpool built for file %s\n\n", file_name);
 
-	void* page_mem = NULL;
-	char data[100]; memset(data, ' ', 100);
-	char* format = "Hello World, This is page number %d -> Buffer pool manager works";
-
 	for(uint32_t i = 0; i < PAGES_IN_HEAP_FILE; i++)
 	{
-		page_mem = get_page_to_write(bpm, i);
-		printf("page %u locked for write\n", i);
-		sprintf(data, format, i);
-		memset(page_mem, ' ', 4096);
-		memcpy(page_mem, data, strlen(data) + 1);
-		printf("Data written on page %u : \t <%s>\n", i, (char*)page_mem);
-		printf("page %u write done\n", i);
-		release_page_write(bpm, page_mem);
-		printf("page %u released from write lock\n\n", i);
+		page_write_and_print(((uint32_t)(i)));
 
 		uint32_t r = (uint32_t)(rand() % (i + 1));
 
-		page_mem = get_page_to_read(bpm, r);
-		printf("page %u locked for read\n", r);
-		printf("Data read from page %u : \t <%s>\n", r, (char*)page_mem);
-		printf("page %u read done\n", r);
-		release_page_read(bpm, page_mem);
-		printf("page %u released from read lock\n\n", r);
+		page_read_and_print(r);
 	}
 
 	delete_bufferpool(bpm);
@@ -56,4 +45,29 @@ int main(int argc, char **argv)
 	printf("test completed\n\n\n");
 
 	return 0;
+}
+
+void page_read_and_print(uint32_t page_id)
+{
+	void* page_mem = get_page_to_read(bpm, page_id);
+	printf("page %u locked for read\n", page_id);
+	printf("Data read from page %u : \t <%s>\n", page_id, (char*)page_mem);
+	printf("page %u read done\n", page_id);
+	release_page_read(bpm, page_mem);
+	printf("page %u released from read lock\n\n", page_id);
+}
+
+void page_write_and_print(uint32_t page_id)
+{
+	int writes = -1;
+	uint32_t page_id_temp = 0;
+	void* page_mem = get_page_to_write(bpm, page_id);
+	printf("page %u locked for write\n", page_id);
+	printf("old Data written on page %u : \t <%s>\n", page_id, (char*)page_mem);
+	sscanf(page_mem, PAGE_DATA_FORMAT, &page_id, &writes);
+	memset(page_mem, ' ', 4096);
+	sprintf(page_mem, PAGE_DATA_FORMAT, page_id, ++writes);
+	printf("new Data written on page %u : \t <%s>\n", page_id, (char*)page_mem);
+	release_page_write(bpm, page_mem);
+	printf("page %u released from write lock\n\n", page_id);
 }
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -4,7 +4,6 @@ cd test
 FILE_NAME="./test.db"
 gcc -o test_bpm.out test_bpm.c -I$BUFFER_POOL_MAN_PATH/inc -I$CUTLERY_PATH/inc -I$RWLOCK_PATH/inc -L$BUFFER_POOL_MAN_PATH/bin -L$CUTLERY_PATH/bin -L$RWLOCK_PATH/bin -lbufferpoolman -lcutlery -lrwlock
 sudo ./test_bpm.out $FILE_NAME
-stat $FILE_NAME
-stat -f $FILE_NAME
-echo "Data written : "
-cat $FILE_NAME
+sudo stat $FILE_NAME
+sudo stat -f $FILE_NAME
+sudo cat $FILE_NAME
