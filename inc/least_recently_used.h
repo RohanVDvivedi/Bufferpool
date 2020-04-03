31028679f93ea6f25e705e4d8326a426423897aa
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-03 22:37:11 +0530

    added functions in lru, that would help in resolving the currently experienced page contention issues

--- inc/least_recently_used.h
+++ inc/least_recently_used.h
@@ -22,6 +22,9 @@ lru* get_lru();
 // you can be assured that the returned replacable page_entry will not exist in the lru,
 page_entry* get_swapable_page(lru* lru_p);
 
+// returns 1, if a given page_entry was removed from the mapping from the lru
+int remove_page_entry_from_lru(lru* lru_p, page_entry* page_ent);
+
 // call this method once you have acquired read or write lock on the page_entry
 void mark_as_recently_used(lru* lru_p, page_entry* page_ent);
 
--- src/least_recently_used.c
+++ src/least_recently_used.c
@@ -23,17 +23,46 @@ page_entry* get_swapable_page(lru* lru_p)
 	return page_ent;
 }
 
-void mark_as_recently_used(lru* lru_p, page_entry* page_ent)
+// if the given page_entry is present in the lru
+// remove the given page_entry from the lru
+static int remove_page_entry_from_lru_if_present_unsafe(lru* lru_p, page_entry* page_ent)
 {
-	pthread_mutex_lock(&(lru_p->page_entries_lock));
-	pthread_mutex_lock(&(page_ent->page_entry_lock));
 	if(page_ent->external_lru_reference != NULL)
 	{
 		remove_node(lru_p->page_entries, page_ent->external_lru_reference);
 		page_ent->external_lru_reference = NULL;
+		return 1;
 	}
-	insert_head(lru_p->page_entries, page_ent);
-	page_ent->external_lru_reference = lru_p->page_entries->head;
+	return 0;
+}
+
+// if the given page_entry is absent in the lru
+// insert the given page_entry to the top of the lru
+static void insert_page_entry_in_lru_head_if_absent_unsafe(lru* lru_p, page_entry* page_ent)
+{
+	if(page_ent->external_lru_reference == NULL)
+	{
+		insert_head(lru_p->page_entries, page_ent);
+		page_ent->external_lru_reference = lru_p->page_entries->head;
+	}
+}
+
+int remove_page_entry_from_lru(lru* lru_p, page_entry* page_ent)
+{
+	pthread_mutex_lock(&(lru_p->page_entries_lock));
+	pthread_mutex_lock(&(page_ent->page_entry_lock));
+		int result = remove_page_entry_from_lru_if_present_unsafe(lru_p, page_ent);
+	pthread_mutex_unlock(&(page_ent->page_entry_lock));
+	pthread_mutex_unlock(&(lru_p->page_entries_lock));
+	return result;
+}
+
+void mark_as_recently_used(lru* lru_p, page_entry* page_ent)
+{
+	pthread_mutex_lock(&(lru_p->page_entries_lock));
+	pthread_mutex_lock(&(page_ent->page_entry_lock));
+		remove_page_entry_from_lru_if_present_unsafe(lru_p, page_ent);
+		insert_page_entry_in_lru_head_if_absent_unsafe(lru_p, page_ent);
 	pthread_mutex_unlock(&(page_ent->page_entry_lock));
 	pthread_mutex_unlock(&(lru_p->page_entries_lock));
 }
