07005cc1bf6e0fd153030ea87a760116ac880f92
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-04-30 14:20:03 +0530

    using var for delay

--- inc/buffer_pool_man_types.h
+++ inc/buffer_pool_man_types.h
@@ -6,6 +6,6 @@
 #include<sys/time.h>
 
 #define setToCurrentUnixTimestamp(var) 		{struct timeval tp;gettimeofday(&tp,NULL);var = tp.tv_sec * 1000 + tp.tv_usec / 1000;}
-#define sleepForMilliseconds(var)			{struct timespec tp;tp.tv_sec = buffp->cleanup_rate_in_milliseconds/1000;tp.tv_nsec = (buffp->cleanup_rate_in_milliseconds % 1000) * 1000000;if(nanosleep(&tp, NULL) == -1){printf("nano sleep failed with %d\n", errno);}}
+#define sleepForMilliseconds(var)			{struct timespec tp;tp.tv_sec = var/1000;tp.tv_nsec = (var%1000) * 1000000;if(nanosleep(&tp, NULL) == -1){printf("nano sleep failed with %d\n", errno);}}
 
 #endif
