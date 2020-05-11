3dfc51b2c55b10012ecccf995ac62b54817117f7
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-05-11 17:58:47 +0530

    added get and delete methods appropriately for the lru

--- src/least_recently_used.c
+++ src/least_recently_used.c
@@ -1,48 +1,12 @@
 #include<least_recently_used.h>
 
-// if the given page_entry is present in the lru
-// remove the given page_entry from the lru
-static int remove_page_entry_from_lru_if_present_unsafe(lru* lru_p, page_entry* page_ent)
-{
-	if(get_by_page_entry(lru_p->node_mapping, page_ent) != NULL)
-	{
-		remove_node(lru_p->page_entries, get_by_page_entry(lru_p->node_mapping, page_ent));
-		set_by_page_entry(lru_p->node_mapping, page_ent, NULL);
-		return 1;
-	}
-	return 0;
-}
-
-// if the given page_entry is absent in the lru
-// insert the given page_entry to the head of the lru
-static void insert_page_entry_in_lru_head_if_absent_unsafe(lru* lru_p, page_entry* page_ent)
-{
-	if(get_by_page_entry(lru_p->node_mapping, page_ent) == NULL)
-	{
-		insert_head(lru_p->page_entries, page_ent);
-		set_by_page_entry(lru_p->node_mapping, page_ent, lru_p->page_entries->head);
-	}
-}
-
-// if the given page_entry is absent in the lru
-// insert the given page_entry to the tail of the lru
-static void insert_page_entry_in_lru_tail_if_absent_unsafe(lru* lru_p, page_entry* page_ent)
-{
-	if(get_by_page_entry(lru_p->node_mapping, page_ent) == NULL)
-	{
-		insert_tail(lru_p->page_entries, page_ent);
-		set_by_page_entry(lru_p->node_mapping, page_ent, lru_p->page_entries->tail);
-	}
-}
-
 lru* get_lru(uint32_t page_entry_count, uint32_t page_size_in_bytes, void* first_page_memory_address)
 {
 	lru* lru_p = (lru*) malloc(sizeof(lru));
 	pthread_cond_init(&(lru_p->wait_for_empty), NULL);
-	lru_p->clean_or_free_page_entries = get_linkedlist(SIMPLE, NULL);
-	lru_p->dirty_page_entries = get_linkedlist(SIMPLE, NULL);
 	pthread_mutex_init(&(lru_p->page_entries_lock), NULL);
-	lru_p->node_mapping = get_page_memory_mapper(first_page_memory_address, page_size_in_bytes, page_entry_count);
+	lru_p->clean_or_free_page_entries = get_page_entry_linkedlist(page_entry_count, page_size_in_bytes, first_page_memory_address);
+	lru_p->dirty_page_entries = get_page_entry_linkedlist(page_entry_count, page_size_in_bytes, first_page_memory_address);
 	return lru_p;
 }
 
@@ -105,8 +69,8 @@ void mark_as_not_yet_used(lru* lru_p, page_entry* page_ent)
 void delete_lru(lru* lru_p)
 {
 	pthread_cond_destroy(&(lru_p->wait_for_empty));
-	delete_linkedlist(lru_p->page_entries);
 	pthread_mutex_destroy(&(lru_p->page_entries_lock));
-	delete_page_memory_mapper(lru_p->node_mapping);
+	delete_page_entry_linkedlist(lru_p->clean_or_free_page_entries);
+	delete_page_entry_linkedlist(lru_p->dirty_page_entries);
 	free(lru_p);
 }
