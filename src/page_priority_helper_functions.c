0504e6240122b07392a4c8f651c4942362a6db74
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-06-06 01:24:45 +0530

    page priority healper funtions built into different source to reuse in page_request

---
+++ src/page_priority_helper_functions.c
@@ -0,0 +1,17 @@
+#include<page_priority_helper_functions.h>
+
+int compare_page_priority(uint8_t page_priority1, uint8_t page_priority2)
+{
+	return compare_unsigned(page_priority1, page_priority2);
+}
+
+void priority_queue_index_change_callback(const void* page_req, unsigned long long int heap_index, const void* additional_params)
+{
+	((page_request*)(page_req))->index_in_priority_queue = heap_index;
+}
+
+// below function can be passed to the for_each of priority queue to increment all the priorities at once
+void priority_increment_wrapper_for_priority_queue(const void* page_req, unsigned long long int heap_index, const void* additional_params)
+{
+	increment_page_request_priority(((page_request*) (page_req)));
+}
