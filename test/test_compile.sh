81a930a9c9fe483e148eed3b103fcd24ea9ae6af
Rohan Dvivedi <rohandvivedi@gmail.com>
2020-08-15 14:31:40 +0530

    the new api does not allow direct access to the dbfile hence need separate compiler and linker flags for compiling the io test, the library name has changed to just bufferpool now

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,2 +1,2 @@
-gcc -o test_bpm.out test_bpm.c -lbufferpoolman -lboompar -lrwlock -lpthread -lcutlery
-gcc -o test_io.out test_io.c -lbufferpoolman -lboompar -lrwlock -lpthread -lcutlery
+gcc -o test_bpm.out test_bpm.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
+#gcc -o test_io.out test_io.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
