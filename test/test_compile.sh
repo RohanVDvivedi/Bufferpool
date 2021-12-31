52167472ba437c45cf6513d5f74aae88ccff8bbc
rohan <rohandvivedi@gmail.com>
2021-12-31 14:07:48 +0530

    testing only the bpm test cases

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
-#gcc -o test_bpm.out test_bpm.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
-#gcc -o test_io.out test_io.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
-gcc -o test_bbq.out test_bbq.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
+gcc -o test_bpm.out test_bpm.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
+#gcc -o test_io.out test_io.c -I../inc -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
+#gcc -o test_bbq.out test_bbq.c -lbufferpool -lboompar -lrwlock -lpthread -lcutlery
