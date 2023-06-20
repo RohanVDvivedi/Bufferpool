171c168307b2afdee2fddc8c66c6d44aa8eddc42
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-20 03:22:00 -0700

    added 2 major structures for the porject revamp bufferpool and page_io_ops

--- inc/bufferpool.h
+++ inc/bufferpool.h
@@ -1,4 +1,44 @@
 #ifndef BUFFERPOOL_H
 #define BUFFERPOOL_H
 
+#include<hashmap.h>
+#include<linkedlist.h>
+
+#include<pthread.h>
+
+typedef struct bufferpool bufferpool;
+struct bufferpool
+{
+	// if set to true, use internal global lock, else use external lock
+	int internal_lock;
+	union
+	{
+		pthread_mutex_t* external_lock;
+		pthread_mutex_t  internal_lock;
+	};
+
+	// max number of frame descriptor count allowed in this bufferpool
+	uint64_t max_frame_desc_count;
+
+	// total number of frame descriptor count
+	uint64_t total_frame_desc_count;
+
+	// hashtable => page_id (uint64_t) -> frame descriptor
+	hashmap page_id_to_frame_desc;
+
+	// hashtable => frame (void*) -> frame descriptor
+	hashmap frame_to_frame_desc;
+
+	// linkedlist of the all frame descriptors that can be written to disk
+	// all locked frames, all frames under IO, all pages that are being waited on by other threads (for read or write lock) are excluded from this list
+	// even if a page is in this list, it is not evicted if the can_be_flushed_to_disk function returns false
+	linkedlist flushable_frame_descs;
+
+	// methods that allow you to read/writes pages to-from secondsa
+	page_io_ops page_io_functions;
+
+	// a page gets flushed to disk only if it passes this test
+	int (*can_be_flushed_to_disk)(uint64_t page_id, const void* frame);
+};
+
 #endif
---
+++ inc/page_io_ops.h
@@ -0,0 +1,21 @@
+#ifndef PAGE_IO_OPS_H
+#define PAGE_IO_OPS_H
+
+// page_io_ops is a structure accepted by the bufferpool, it is interface how to read, write pages to underlying storage
+
+typedef struct page_io_ops page_io_ops;
+struct page_io_ops
+{
+	const void* page_io_ops_handle;
+
+	// read page from disk at page_id into the frame pointed by frame_dest
+	int read_page(const void* page_io_ops_handle, void* frame_dest, uint64_t page_id, uint32_t page_size);
+
+	// write page contents pointed to by frame_src to the disk at page_id
+	int write_page(const void* page_io_ops_handle, const void* frame_src, uint64_t page_id, uint32_t page_size);
+
+	// flush all write to underlying disk, all writes are assumed to be persistent after this call returns successfully
+	int flush_all_writes(const void* page_io_ops_handle);
+};
+
+#endif
