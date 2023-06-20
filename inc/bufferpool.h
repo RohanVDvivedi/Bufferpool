962c816354532044409ae5c79be497ef99e9701f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-20 08:05:47 -0700

    added more comments to explain the work of different flags

--- inc/bufferpool.h
+++ inc/bufferpool.h
@@ -56,8 +56,18 @@ void deinitialize_bufferpool(bufferpool* bf);
 const void* get_page_with_reader_lock(bufferpool* bf, uint64_t page_id);
 int release_reader_lock_on_page(bufferpool* bf, const void* frame);
 
-void* get_page_with_writer_lock(bufferpool* bf, uint64_t page_id, int without_read_IO);
+void* get_page_with_writer_lock(bufferpool* bf, uint64_t page_id, int to_be_overwritten);
 int release_writer_lock_on_page(bufferpool* bf, void* frame, int was_modified, int force_flush);
+
+/*
+	flags information :
+		to_be_overwritten -> If the page frame at page_id is not in bufferpool, even then the page is not read from disk
+		                  -> This can be used, if you are sure that the page had not been used or allocated prior to this call
+		                  -> this flag is a NO-OP if the page is already in bufferpool
+
+		was_modified -> this bit suggests if the page was dirtied by the user, dirty_bit = dirty_bit || was_modified
+		floce_flush  -> the call returns only after writing and flushing the page to disk
+*/
 
 int downgrade_writer_lock_to_reader_lock(bufferpool* bf, void* frame);
 int upgrade_reader_lock_to_writer_lock(bufferpool* bf, void* frame);
