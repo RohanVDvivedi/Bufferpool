73cc5a4d2c7dc4e4a144daa4c0094941e974d455
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-21 06:34:36 -0700

    added find and insert, update and remove functions for frame_desc based on the values of page_id and frame ptr

--- src/bufferpool_util.c
+++ src/bufferpool_util.c
@@ -6,3 +6,32 @@ pthread_mutex_t* get_bufferpool_lock(bufferpool* bf)
 		return &(bf->internal_lock);
 	return bf->external_lock;
 }
+
+int insert_frame_desc(bufferpool* bf, frame_desc* fd)
+{
+	return insert_in_hashmap(&(bf->page_id_to_frame_desc), fd) && insert_in_hashmap(&(bf->frame_ptr_to_frame_desc), fd);
+}
+
+int update_page_id_for_frame_desc(bufferpool* bf, frame_desc* fd, uint64_t new_page_id)
+{
+	int removed = remove_from_hashmap(&(bf->page_id_to_frame_desc), fd);
+	if(!removed)
+		return 0;
+	fd->page_id = new_page_id;
+	return insert_in_hashmap(&(bf->page_id_to_frame_desc), fd);
+}
+
+int remove_frame_desc(bufferpool* bf, frame_desc* fd)
+{
+	return remove_from_hashmap(&(bf->page_id_to_frame_desc), fd) && remove_from_hashmap(&(bf->frame_ptr_to_frame_desc), fd);
+}
+
+frame_desc* find_frame_desc_by_page_id(bufferpool* bf, uint64_t page_id)
+{
+	return (frame_desc*) find_equals_in_hashmap(&(bf->page_id_to_frame_desc), &((const frame_desc){.page_id = page_id}));
+}
+
+frame_desc* find_frame_desc_by_frame_ptr(bufferpool* bf, void* frame)
+{
+	return (frame_desc*) find_equals_in_hashmap(&(bf->frame_ptr_to_frame_desc), &((const frame_desc){.frame = frame}));
+}
