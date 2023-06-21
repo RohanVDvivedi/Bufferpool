f6501fb9a0a6b443a916829e15b7f285221d0bee
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-21 07:35:53 -0700

    started with implementing logic for locking routines

--- src/bufferpool_lock.c
+++ src/bufferpool_lock.c
@@ -1,13 +1,67 @@
 #include<bufferpool.h>
 
-void* get_page_with_reader_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary);
+void* get_page_with_reader_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
 
-void* get_page_with_writer_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary, int to_be_overwritten);
+	// TODO
 
-int downgrade_writer_lock_to_reader_lock(bufferpool* bf, void* frame, int was_modified, int force_flush);
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
 
-int upgrade_reader_lock_to_writer_lock(bufferpool* bf, void* frame);
+void* get_page_with_writer_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary, int to_be_overwritten)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
 
-int release_reader_lock_on_page(bufferpool* bf, void* frame);
+	// TODO
 
-int release_writer_lock_on_page(bufferpool* bf, void* frame, int was_modified, int force_flush);
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
+
+int downgrade_writer_lock_to_reader_lock(bufferpool* bf, void* frame, int was_modified, int force_flush)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
+
+	// TODO
+
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
+
+int upgrade_reader_lock_to_writer_lock(bufferpool* bf, void* frame)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
+
+	// TODO
+
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
+
+int release_reader_lock_on_page(bufferpool* bf, void* frame)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
+
+	// TODO
+
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
+
+int release_writer_lock_on_page(bufferpool* bf, void* frame, int was_modified, int force_flush)
+{
+	if(bf->has_internal_lock)
+		pthread_mutex_lock(get_bufferpool_lock(bf));
+
+	// TODO
+
+	if(bf->has_internal_lock)
+		pthread_mutex_unlock(get_bufferpool_lock(bf));
+}
