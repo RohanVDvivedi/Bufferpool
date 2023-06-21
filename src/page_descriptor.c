16a1eecd470303cebcbbf0193e7cdf71814f3a81
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-21 03:44:50 -0700

    initialized conceptual function to check if the page is being written or read

--- src/page_descriptor.c
+++ src/page_descriptor.c
@@ -1,1 +1,15 @@
 #include<page_descriptor.h>
+
+page_desc* new_page_desc();
+
+void delete_page_desc(page_desc* pd_p);
+
+uint64_t get_total_readers_count_on_page_desc(page_desc* pd_p)
+{
+	return pd_p->readers_count + pd_p->is_under_write_IO;
+}
+
+uint64_t get_total_writers_count_on_page_desc(page_desc* pd_p)
+{
+	return pd_p->writers_count + pd_p->is_under_read_IO;
+}
