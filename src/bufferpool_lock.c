8c9895996fa5cfe9d953e876f0095ba152a68d0e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-21 07:33:28 -0700

    started implementing locking routines

--- src/bufferpool_lock.c
+++ src/bufferpool_lock.c
@@ -1,1 +1,13 @@
 #include<bufferpool.h>
+
+void* get_page_with_reader_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary);
+
+void* get_page_with_writer_lock(bufferpool* bf, uint64_t page_id, int evict_dirty_if_necessary, int to_be_overwritten);
+
+int downgrade_writer_lock_to_reader_lock(bufferpool* bf, void* frame, int was_modified, int force_flush);
+
+int upgrade_reader_lock_to_writer_lock(bufferpool* bf, void* frame);
+
+int release_reader_lock_on_page(bufferpool* bf, void* frame);
+
+int release_writer_lock_on_page(bufferpool* bf, void* frame, int was_modified, int force_flush);
