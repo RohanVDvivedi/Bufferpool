cfa40ebcb317a454a8b3bb59256f83063bf93c0a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-23 06:04:06 -0700

    major bug fix

--- src/frame_descriptor_util.c
+++ src/frame_descriptor_util.c
@@ -9,7 +9,7 @@ cy_uint hash_frame_desc_by_page_id(const void* pd_p)
 
 int compare_frame_desc_by_page_id(const void* pd1_p, const void* pd2_p)
 {
-	return compare(((const frame_desc*)pd1_p)->page_id, ((const frame_desc*)pd1_p)->page_id);
+	return compare(((const frame_desc*)pd1_p)->page_id, ((const frame_desc*)pd2_p)->page_id);
 }
 
 cy_uint hash_frame_desc_by_frame_ptr(const void* pd_p)
@@ -19,5 +19,5 @@ cy_uint hash_frame_desc_by_frame_ptr(const void* pd_p)
 
 int compare_frame_desc_by_frame_ptr(const void* pd1_p, const void* pd2_p)
 {
-	return compare(((const frame_desc*)pd1_p)->frame, ((const frame_desc*)pd1_p)->frame);
+	return compare(((const frame_desc*)pd1_p)->frame, ((const frame_desc*)pd2_p)->frame);
 }
