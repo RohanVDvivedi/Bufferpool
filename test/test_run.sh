c052d016287acf0d8c5aa8ee530af95087bf8662
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-22 23:18:01 -0700

    revamping test folder

--- test/test_run.sh
+++ test/test_run.sh
@@ -7,12 +7,5 @@ then
 	sudo time -v ./test_bpm.out $FILENAME
 	sudo stat $FILENAME
 	sudo stat -f $FILENAME
-	sudo head -c `expr 512 \* 8 \* 20` $FILENAME
-elif [ $TEST_TYP = "io_speed" ]
-then
-	sudo time -v ./test_io.out $FILENAME 160 8
-	sudo time -v ./test_io.out $FILENAME 640 8
-	echo "1. Even on SSD sequential io is slower"
-	echo "2. With larger amount of data for io, the difference in sequential and random io increases"
-	echo "3. Always try to make fewer and bigger read/write calls for disk and execution effeciency"
+	sudo cat -c `expr 512 \* 8 \* 20` $FILENAME
 fi
