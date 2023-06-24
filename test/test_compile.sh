ab187557554c42e5a6755733dcc6802dfaf96cd9
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-23 22:17:04 -0700

    updating test file name

--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
 gcc -o test_bpm.out test_bpm.c blockfile_page_io_ops_test_util.c -lblockio -lboompar -lbufferpool -lpthread -lcutlery
 
-gcc -o test_bpm_rnu.out test_bpm_readers_and_upgraders.c blockfile_page_io_ops_test_util.c -lblockio -lboompar -lbufferpool -lpthread -lcutlery
+gcc -o test_bpm_rnu.out test_bpm_rnu.c blockfile_page_io_ops_test_util.c -lblockio -lboompar -lbufferpool -lpthread -lcutlery
