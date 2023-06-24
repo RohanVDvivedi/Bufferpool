4f07fc3cf043d9ebf98bb2804f9adf19e2843304
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-24 07:43:41 -0700

    tests passing

--- test/test_bpm.c
+++ test/test_bpm.c
@@ -122,6 +122,9 @@ int main(int argc, char **argv)
 		}
 	}
 
+	// flush everything, this make initialization complete
+	flush_all_possible_dirty_pages(&bpm);
+
 	executor* exe = new_executor(FIXED_THREAD_COUNT_EXECUTOR, FIXED_THREAD_POOL_SIZE, COUNT_OF_IO_TASKS + 32, 0, NULL, NULL, NULL);
 	printf("Executor service started to simulate multiple concurrent io of %d io tasks among %d threads\n\n", COUNT_OF_IO_TASKS, FIXED_THREAD_POOL_SIZE);
 
