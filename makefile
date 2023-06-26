5c129279d45bce9cf8e94f22109ea6388ca62d3e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-26 10:26:07 -0700

    fixed linking order

--- makefile
+++ makefile
@@ -25,7 +25,7 @@ CC:=gcc
 # compiler flags
 CFLAGS:=-Wall -O3 -I${INC_DIR}
 # linker flags, this will used to compile the binary
-LFLAGS:=-L${LIB_DIR} -l${PROJECT_NAME} -lpthread -lcutlery
+LFLAGS:=-L${LIB_DIR} -l${PROJECT_NAME} -lboompar -lpthread -lcutlery
 # Archiver
 AR:=ar rcs
 
--- test/test_compile.sh
+++ test/test_compile.sh
@@ -1,3 +1,3 @@
-gcc -o test_bpm.out test_bpm.c blockfile_page_io_ops_test_util.c -lblockio -lboompar -lbufferpool -lpthread -lcutlery
+gcc -o test_bpm.out test_bpm.c blockfile_page_io_ops_test_util.c -lblockio -lbufferpool -lboompar -lpthread -lcutlery
 
-gcc -o test_bpm_rnu.out test_bpm_rnu.c blockfile_page_io_ops_test_util.c -lblockio -lboompar -lbufferpool -lpthread -lcutlery
+gcc -o test_bpm_rnu.out test_bpm_rnu.c blockfile_page_io_ops_test_util.c -lblockio -lbufferpool -lboompar -lpthread -lcutlery
