c9e04138e11dee6b155ea97eedfa1fab1766edbe
RohanVDvivedi <rohandvivedi@gmail.com>
2023-07-04 02:47:39 -0700

    added static where necessary

--- test/blockfile_page_io_ops_test_util.c
+++ test/blockfile_page_io_ops_test_util.c
@@ -6,7 +6,7 @@
 
 #include<page_io_ops.h>
 
-int read_page_from_block_file(const void* page_io_ops_handle, void* frame_dest, uint64_t page_id, uint32_t page_size)
+static int read_page_from_block_file(const void* page_io_ops_handle, void* frame_dest, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
 	off_t block_id = (page_id * page_size) / block_size;
@@ -15,7 +15,7 @@ int read_page_from_block_file(const void* page_io_ops_handle, void* frame_dest, 
 	return read_blocks_from_block_file(((block_file*)(page_io_ops_handle)), frame_dest, block_id, block_count);
 }
 
-int write_page_to_block_file(const void* page_io_ops_handle, const void* frame_src, uint64_t page_id, uint32_t page_size)
+static int write_page_to_block_file(const void* page_io_ops_handle, const void* frame_src, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
 	off_t block_id = (page_id * page_size) / block_size;
@@ -24,7 +24,7 @@ int write_page_to_block_file(const void* page_io_ops_handle, const void* frame_s
 	return write_blocks_to_block_file(((block_file*)(page_io_ops_handle)), frame_src, block_id, block_count);
 }
 
-int flush_all_pages_to_block_file(const void* page_io_ops_handle)
+static int flush_all_pages_to_block_file(const void* page_io_ops_handle)
 {
 	return flush_all_writes_to_block_file(((block_file*)(page_io_ops_handle)));
 }
