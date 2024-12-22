cd1823544cc2f6e04f405cad82724f55c1c61b75
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-22 20:25:11 +0530

    using a randomizer to easily mask the linear relation ship in the page_ids and alignment relations ship in frame pointers

--- src/frame_descriptor_util.c
+++ src/frame_descriptor_util.c
@@ -4,7 +4,7 @@
 
 cy_uint hash_frame_desc_by_page_id(const void* pd_p)
 {
-	return ((const frame_desc*)pd_p)->map.page_id;
+	return hash_randomizer( ((const frame_desc*)pd_p)->map.page_id );
 }
 
 int compare_frame_desc_by_page_id(const void* pd1_p, const void* pd2_p)
@@ -14,7 +14,7 @@ int compare_frame_desc_by_page_id(const void* pd1_p, const void* pd2_p)
 
 cy_uint hash_frame_desc_by_frame_ptr(const void* pd_p)
 {
-	return (cy_uint)(((const frame_desc*)pd_p)->map.frame);
+	return hash_randomizer( (cy_uint)(((const frame_desc*)pd_p)->map.frame) );
 }
 
 int compare_frame_desc_by_frame_ptr(const void* pd1_p, const void* pd2_p)
