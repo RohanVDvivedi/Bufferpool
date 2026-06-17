819f128002a4d7cfdfb2074fe0ae32f217e9fed4
RohanVDvivedi <rohandvivedi@gmail.com>
2026-06-18 00:11:39 +0530

    --

--- src/bufferpool_init.c
+++ src/bufferpool_init.c
@@ -102,7 +102,9 @@ static void on_remove_all_from_page_id_to_frame_desc_hashmap_delete_frame_from_b
 
 void deinitialize_bufferpool(bufferpool* bf)
 {
-	// this function does the shutdown of the periodic job
+	// this sequence does the shutdown of the periodic job and deletes it
+	shutdown_periodic_job(mbf->periodic_flush_job);
+	wait_for_pause_or_shutdown_of_periodic_job(bf->periodic_flush_job);
 	delete_periodic_job(bf->periodic_flush_job);
 
 	shutdown_executor(bf->cached_threadpool_executor, 0);
