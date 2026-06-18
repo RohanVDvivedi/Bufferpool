15b3ab70d4501b9d8898b1724ea2aa50a72276e9
RohanVDvivedi <rohandvivedi@gmail.com>
2026-06-18 20:22:50 +0530

    portability issue

--- test/blockfile_page_io_ops_test_util.c
+++ test/blockfile_page_io_ops_test_util.c
@@ -9,7 +9,7 @@
 static int read_page_from_block_file(const void* page_io_ops_handle, void* frame_dest, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
-	off_t block_id = (page_id * page_size) / block_size;
+	uint64_t block_id = (page_id * page_size) / block_size;
 	size_t block_count = page_size / block_size;
 	printf("reading %"PRIu64" - %"PRIu64" into  %p\n", block_id, block_id + block_count - 1, frame_dest);
 	return read_blocks_from_block_file(((block_file*)(page_io_ops_handle)), frame_dest, block_id, block_count);
@@ -18,7 +18,7 @@ static int read_page_from_block_file(const void* page_io_ops_handle, void* frame
 static int write_page_to_block_file(const void* page_io_ops_handle, const void* frame_src, uint64_t page_id, uint32_t page_size)
 {
 	size_t block_size = get_block_size_for_block_file(((block_file*)(page_io_ops_handle)));
-	off_t block_id = (page_id * page_size) / block_size;
+	uint64_t block_id = (page_id * page_size) / block_size;
 	size_t block_count = page_size / block_size;
 	printf("writing %"PRIu64" - %"PRIu64" from  %p\n", block_id, block_id + block_count - 1, frame_src);
 	return write_blocks_to_block_file(((block_file*)(page_io_ops_handle)), frame_src, block_id, block_count);
